488ed06cc95c7599be4bdf47f2ad38b8e823abad
x.nep <57833538+zenss555a@users.noreply.github.com>
2026-01-16 23:21:48 +0700

    Update Prison-Life.lua

--- Prison-Life.lua
+++ Prison-Life.lua
@@ -1,2 +1,2 @@
 getgenv().SCRIPT_KEY = "KEYLESS"
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/47a6bfd4daf4be4ce1c2951d47971decb2ce32e087e45adb171dc642629a7619/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2508395c90052ffd288fa02381df3d685b3ef520411ea31d1266c92f3c52e605/download"))()
