f894773a9f1261c8e4dddafd829c95d4c8ea68c7
x.nep <57833538+zenss555a@users.noreply.github.com>
2026-01-24 23:50:59 +0700

    Delete Prison-Life.lua

--- Prison-Life.lua
+++
@@ -1,2 +0,0 @@
-getgenv().SCRIPT_KEY = "KEYLESS"
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2508395c90052ffd288fa02381df3d685b3ef520411ea31d1266c92f3c52e605/download"))()
