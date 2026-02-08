40346986c261eba63999aeed2a416b96bf7b5651
x.nep <57833538+zenss555a@users.noreply.github.com>
2026-02-08 14:22:06 +0700

    Update Prison-Life.lua

--- Prison-Life.lua
+++ Prison-Life.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2508395c90052ffd288fa02381df3d685b3ef520411ea31d1266c92f3c52e605/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/7190069da5cf5e79b2d88dc3e7e51a734971808011659b1f1197369703663f01/download"))()
